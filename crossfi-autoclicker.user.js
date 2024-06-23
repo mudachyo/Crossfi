1e272321ca6bf0412d935f8df5c93c4311dfdbc7
Daniil Voroshin <kotanoff@raikage.ru>
2024-06-23 17:26:00 +0300

    🚂 upload autoclicker

---
+++ crossfi-autoclicker.user.js
@@ -0,0 +1,128 @@
+// ==UserScript==
+// @name         Crossfi Autoclicker
+// @version      1.0
+// @author       mudachyo
+// @match        *://bot.crossfi.org/*
+// @icon         https://s2.coinmarketcap.com/static/img/coins/64x64/26202.png
+// @grant        none
+// @downloadURL  https://github.com/mudachyo/Сrossfi/raw/main/crossfi-autoclicker.user.js
+// @updateURL    https://github.com/mudachyo/Сrossfi/raw/main/crossfi-autoclicker.user.js
+// @homepage     https://github.com/mudachyo/Сrossfi
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const styles = {
+        success: 'background: #28a745; color: #fff; font-weight: bold; padding: 4px 8px; border-radius: 4px;',
+        starting: 'background: #8640ff; color: #fff; font-weight: bold; padding: 4px 8px; border-radius: 4px;',
+        error: 'background: #dc3545; color: #fff; font-weight: bold; padding: 4px 8px; border-radius: 4px;',
+        info: 'background: #007bff; color: #fff; font-weight: bold; padding: 4px 8px; border-radius: 4px;'
+    };
+
+    const logPrefix = '%c[CrossfiBot] ';
+    const originalLog = console.log;
+    console.log = function () {
+        if (typeof arguments[0] === 'string' && arguments[0].includes('[CrossfiBot]')) {
+            originalLog.apply(console, arguments);
+        }
+    };
+
+    console.error = console.warn = console.info = console.debug = () => {};
+
+    console.clear();
+    console.log(`${logPrefix}Starting`, styles.starting);
+    console.log(`${logPrefix}Created by https://t.me/mudachyo`, styles.starting);
+    console.log(`${logPrefix}Github https://github.com/mudachyo/Crossfi`, styles.starting);
+
+    let autoClickerInterval;
+    const energySelector = '.MuiTypography-root.MuiTypography-body1.energyCount.css-ex96t';
+
+    function getRandomInterval() {
+        return Math.floor(Math.random() * (120 - 30 + 1)) + 30;
+    }
+
+    function getRandomCoordinate(radius) {
+        let angle = Math.random() * 2 * Math.PI;
+        let r = Math.sqrt(Math.random()) * radius;
+        let x = r * Math.cos(angle);
+        let y = r * Math.sin(angle);
+        return { x, y };
+    }
+
+    function createPointerEvent(type, x, y) {
+        return new PointerEvent(type, {
+            bubbles: true,
+            cancelable: true,
+            clientX: x,
+            clientY: y,
+            pointerId: 1,
+            width: 1,
+            height: 1,
+            pressure: type === 'pointerdown' ? 0.5 : 0,
+            isPrimary: true
+        });
+    }
+
+    function createMouseEvent(type, x, y) {
+        return new MouseEvent(type, {
+            bubbles: true,
+            cancelable: true,
+            clientX: x,
+            clientY: y
+        });
+    }
+
+    function clickCanvas() {
+        try {
+            const canvas = document.querySelector('.tapCoinCanvas');
+            if (canvas) {
+                const rect = canvas.getBoundingClientRect();
+                const radius = rect.width / 2;
+                const centerX = rect.left + radius;
+                const centerY = rect.top + radius;
+
+                const randomCoord = getRandomCoordinate(radius);
+                const x = centerX + randomCoord.x;
+                const y = centerY + randomCoord.y;
+
+                canvas.dispatchEvent(createPointerEvent('pointerdown', x, y));
+                canvas.dispatchEvent(createMouseEvent('mousedown', x, y));
+                canvas.dispatchEvent(createPointerEvent('pointerup', x, y));
+                canvas.dispatchEvent(createMouseEvent('mouseup', x, y));
+                canvas.dispatchEvent(createPointerEvent('click', x, y));
+            }
+        } catch (error) {
+            console.log(`${logPrefix}Error clicking the canvas: ${error}`, styles.error);
+        }
+    }
+
+    function getEnergy() {
+        const energyElement = document.querySelector(energySelector);
+        return energyElement ? parseInt(energyElement.textContent, 10) : null;
+    }
+
+    function pauseAutoClicker() {
+        clearInterval(autoClickerInterval);
+        const pauseDuration = Math.floor(Math.random() * (60 - 30 + 1)) + 30;
+        console.log(`${logPrefix}Pausing for ${pauseDuration} seconds due to low energy`, styles.info);
+        setTimeout(startAutoClicker, pauseDuration * 1000);
+    }
+
+    function startAutoClicker() {
+        autoClickerInterval = setInterval(() => {
+            const energy = getEnergy();
+            if (energy !== null && energy < 25) {
+                pauseAutoClicker();
+            } else {
+                clickCanvas();
+            }
+        }, getRandomInterval());
+    }
+
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            startAutoClicker();
+        }, 5000);
+    });
+})();
---
+++ crossfi-web.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Сrossfi web
+// @version      1.0
+// @author       mudachyo
+// @match        *://bot.crossfi.org/*
+// @run-at       document-start
+// @icon         https://s2.coinmarketcap.com/static/img/coins/64x64/26202.png
+// @grant        none
+// @downloadURL  https://github.com/mudachyo/Сrossfi/raw/main/crossfi-web.user.js
+// @updateURL    https://github.com/mudachyo/Сrossfi/raw/main/crossfi-web.user.js
+// @homepage     https://github.com/mudachyo/Сrossfi
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var newUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
+
+    Object.defineProperty(navigator, 'userAgent', {
+        get: function() { return newUserAgent; }
+    });
+
+    Object.defineProperty(navigator, 'platform', {
+        get: function() { return 'iPhone'; }
+    });
+
+    Object.defineProperty(navigator, 'vendor', {
+        get: function() { return 'Apple Computer, Inc.'; }
+    });
+
+})();
